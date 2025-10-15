783986c87c00672a1fc9a4a0d78ab8bda529014b
BrendenHorne <160657026+BrendenHorne@users.noreply.github.com>
2025-10-15 15:22:31 -0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -209,15 +209,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files
 STATIC_URL = '/static/'
 
-# Where collectstatic will put files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra static files (like your frontend build)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist'),
+    os.path.join(BASE_DIR, '../frontend/dist'),
 ]
 
-# WhiteNoise for production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
